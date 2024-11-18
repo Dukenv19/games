c21fa04ba85e65aeea2c5a64e2a2cc6cdd6be2a6
Dukenv19 <167692807+Dukenv19@users.noreply.github.com>
2024-11-18 15:52:33 +0900

    Add files via upload

---
+++ PG15/README.md
@@ -0,0 +1,12 @@
+# Puzzle Game 15
+Puzzle game "15" using JQuery and Bootstrap 4
+
+###
+![png](./img/puzzle.png)
+
+## Live Demo
+[Puzzle Game 15 demo](https://andriival.github.io/PuzzleGame15)
+
+###
+![png](./img/finish.png)
+![png](./img/won.png)
---
+++ WordsGame/scripts/word-list.js
@@ -0,0 +1,158 @@
+const wordList = [
+    {
+        word: "tyrannosaurus",
+        hint: "This dinosaur is known as the 'king of the dinosaurs.' It has short arms but a very large head and sharp teeth."
+    },
+    {
+        word: "stegosaurus",
+        hint: "This dinosaur has plates on its back and spikes on its tail. It's famous for its distinctive and unique appearance."
+    },
+    {
+        word: "triceratops",
+        hint: "Imagine a dinosaur with three horns on its face, like a rhinoceros but from the prehistoric times."
+    },
+    {
+        word: "velociraptor",
+        hint: "This dinosaur is small and fast. It's often depicted as a clever predator with sharp claws."
+    },
+    {
+        word: "brachiosaurus",
+        hint: "Think of a dinosaur with a long neck that reaches high into the trees. It's a giant, long-necked herbivore."
+    },
+    {
+        word: "pterodactyl",
+        hint: "This is not exactly a dinosaur but a flying reptile from the same era. It has a wingspan and beak like a bird."
+    },
+    {
+        word: "ankylosaurus",
+        hint: "Picture a dinosaur with armor-like plates and a clubbed tail. It's like a walking tank from the past."
+    },
+    {
+        word: "diplodocus",
+        hint: "Imagine a dinosaur with an extremely long neck and tail. It's a gentle giant, munching on leaves from tall trees."
+    },
+    {
+        word: "parasaurolophus",
+        hint: "This dinosaur has a crest on its head that looks like a long tube. It might remind you of a musical instrument."
+    },
+    {
+        word: "allosaurus",
+        hint: "Think of a large carnivorous dinosaur with sharp teeth and a powerful jaw. It was a fearsome predator in its time."
+    },
+    {
+        word: "iguanodon",
+        hint: "Imagine a dinosaur that resembles a modern iguana but much larger. It's a herbivore with thumb spikes."
+    },
+    {
+        word: "spinosaurus",
+        hint: "This dinosaur is known for its sail-like structure on its back. It was a massive predator, even larger than T. rex."
+    },
+    {
+        word: "futabasaurus",
+        hint: "Imagine a dinosaur with a long neck and aquatic features. It's a marine reptile that lived during the early Cretaceous period."
+    },
+    {
+        word: "argentinosaurus",
+        hint: "Think of a dinosaur with an incredibly long neck and tail. It's one of the largest sauropod dinosaurs and was a massive, long-necked herbivore."
+    },
+    {
+        word: "kentrosaurus",
+        hint: "Picture a dinosaur with sharp, pointed plates along its back and long spikes on its tail. It's a smaller relative of Stegosaurus and lived during the Late Jurassic period."
+    },
+    {
+        word: "pteranodon",
+        hint: "This is not exactly a dinosaur but a flying reptile. It has a long wingspan, a crest on its head, and is often depicted as soaring through the prehistoric skies."
+    },
+    {
+        word: "pachycephalosaurus",
+        hint: "Think of a dinosaur with a thick, bony dome on its head. It's known for head-butting, like a prehistoric battering ram."
+    },
+    {
+        word: "suchomimus",
+        hint: "Imagine a dinosaur with a long snout and crocodile-like features. It's a large theropod dinosaur that lived during the Cretaceous period."
+    },
+    {
+        word: "anchiornis",
+        hint: "Picture a small, feathered dinosaur that lived during the Late Jurassic period. It's known for its bird-like features and colorful plumage."
+    },
+    {
+        word: "eoraptor",
+        hint: "Imagine one of the earliest dinosaurs, a small and carnivorous dinosaur from the Late Triassic period. Its name means 'dawn thief.'"
+    },
+    {
+        word: "torosaurus",
+        hint: "Think of a dinosaur with a large frill and long horns on its frill. It's a close relative of Triceratops and lived during the Late Cretaceous period."
+    },
+    {
+        word: "fossil",
+        hint: "This is not a dinosaur, but it's what remains of a prehistoric organism preserved in rock. Scientists study these to learn about ancient life."
+    },
+    {
+        word: "paleontologist",
+        hint: "This is a scientist who studies fossils and prehistoric life. They often go on expeditions to unearth and understand the secrets of ancient times."
+    },
+    {
+        word: "skeleton",
+        hint: "It's the framework of bones that supports an animal's body. When we talk about dinosaurs, we often discover their history through the fossils."
+    },
+    {
+        word: "prey",
+        hint: "Animals that are eaten by predators."
+    },
+    {
+        word: "predator",
+        hint: "An animal that hunts and kills other animals for food."
+    },
+    {
+        word: "extinct",
+        hint: "When a group of animals or plants completely dies out."
+    },
+    {
+        word: "museum",
+        hint: "This is a place where you can see and learn about fossils, including dinosaur skeletons. It's like a time capsule that preserves the wonders of the past."
+    },
+    {
+        word: "mammal",
+        hint: "Warm-booded animals that drink their mother's milk as babies."
+    },
+    {
+        word: "carnivore",
+        hint: "This word describes dinosaurs that primarily ate meat. Think of dinosaurs like Tyrannosaurus Rex, which was a famous dinosaur of this type."
+    },
+    {
+        word: "herbivore",
+        hint: "This term describes dinosaurs that mainly ate plants. Picture dinosaurs like Brachiosaurus or Triceratops, which grazed on vegetation."
+    },
+    {
+        word: "excavation",
+        hint: "It's like carefully digging in the ground to find and discover old bones and treasures from a long, long time ago."
+    },
+    {
+        word: "amber",
+        hint: "Sometimes, we find insects or even tiny dinosaurs trapped inside this shiny, orange-yellow substance."
+    },
+    {
+        word: "prehistory",
+        hint: "Imagine a time so long ago that there were no written records. It's like looking back at the story of Earth and dinosaurs before people started writing things down."
+    },
+    {
+        word: "desert",
+        hint: "Picture a place with lots of sand and very little water. Some dinosaurs lived in places like this, where it was hot and dry, with not many plants around."
+    },
+    {
+        word: "amphibian",
+        hint: "Animal that can live both in water and on land. It's like a creature that can splash around in ponds but also explore the land, just like some ancient animals did."
+    }, 
+    {
+        word: "asteroid",
+        hint: "Imagine a big rock from space. It's like a space rock that played a part in the dinosaur story."
+    },
+    {
+        word: "armor",
+        hint: "This is a tough, protective covering. Some dinosaurs had natural shields that helped keep them safe in a world full of challenges."
+    },
+    {
+        word: "reptile",
+        hint: "Imagine a scaly friend from long ago. Some dinosaurs were like big, ancient creatures—covered in scales and sometimes moving slow and steady, just like animals you might see today."
+    },
+];
